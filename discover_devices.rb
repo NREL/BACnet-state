2c135fc5118b115a5e63a3412aebdb4d1385d411
Anya Petersen <anyaelena@gmail.com>
2013-12-12 14:23:58 -0700

    Request and persist extended device properties during discovery.

--- discover_devices.rb
+++ discover_devices.rb
@@ -1,9 +1,9 @@
 require 'config/initialize.rb'
 
-# TODO move into discoverer class
 config = gov.nrel.bacnet.consumer.BACnet.parseOptions(ARGV)
 bacnet = gov.nrel.bacnet.consumer.BACnet.new(config)
 $local_device = bacnet.getLocalDevice
+KnownDevice.set_local_device($local_device)
 discoverer = Discoverer.new(config.getMinId, config.getMaxId, $local_device)
 discoverer.schedule_broadcast
 
