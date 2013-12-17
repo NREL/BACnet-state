37046b1346555b321a2ee69c77570c53b5c73e4f
Anya Petersen <anyaelena@gmail.com>
2013-12-17 10:24:20 -0700

    Move Oid filtering logic into KnownDevice.

--- models/device_oid_lookup.rb
+++ models/device_oid_lookup.rb
@@ -1,14 +1,16 @@
 class DeviceOidLookup
   include java.lang.Runnable
-  def initialize known_device, local_device
+  def initialize known_device, local_device, filters 
     @device = known_device
     @local_device = local_device
+    @filters = filters
   end
   def run
     begin
       puts "starting to process #{@device.instance_number}"
       start = Time.now.to_i
       @device.discover_oids @local_device
+      @device.apply_oid_filters @filters
       puts "finished processing #{@device.instance_number} in #{Time.now.to_i - start} milliseconds"
     rescue Exception => e 
       LoggerSingleton.logger.error "oid scan for device #{@device.instance_number} failed with error #{e.to_s}"
