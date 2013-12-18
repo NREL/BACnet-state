73e20b7c1db009e395c68e80a1e383158a05a2e6
Anya Petersen <anyaelena@gmail.com>
2013-12-18 14:02:22 -0700

    Use OurExecutor for managing threadpools.

--- discover_devices.rb
+++ discover_devices.rb
@@ -4,6 +4,6 @@ config = gov.nrel.bacnet.consumer.BACnet.parseOptions(ARGV)
 bacnet = gov.nrel.bacnet.consumer.BACnet.new(config)
 $local_device = bacnet.getLocalDevice
 KnownDevice.set_local_device($local_device)
-discoverer = Discoverer.new(config.getMinId, config.getMaxId, $local_device, bacnet.getSchedSvc)
+discoverer = Discoverer.new(config.getMinId, config.getMaxId, $local_device, bacnet.getOurExec.getScheduledSvc)
 discoverer.schedule_broadcast
 
--- discover_oids.rb
+++ discover_oids.rb
@@ -1,16 +1,15 @@
 require 'config/initialize.rb'
-pool_size = 10
-
-# partitions = Java::JavaUtil::ArrayList.new
-
-# TODO move into discoverer class
 config = gov.nrel.bacnet.consumer.BACnet.parseOptions(ARGV)
 bacnet = gov.nrel.bacnet.consumer.BACnet.new(config)
-executorPool = bacnet.getPollingSchedSvc
+puts "config databusenabled: #{config.getDatabusEnabled}"
+# our executor has queue management
+executorPool = bacnet.getOurExec
 local_device = bacnet.getLocalDevice
 filters = bacnet.getFilters
-KnownDevice.all.each do |kd|
-  # does not save timeout
-  executorPool.execute(DeviceOidLookup.new(kd, local_device, filters))
+writer = bacnet.getDatabusDataWriter
+puts "writer: #{writer.class}"
 
+
+KnownDevice.all.each do |kd|
+  executorPool.execute(DeviceOidLookup.new(kd, local_device, filters))
 end
