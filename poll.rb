181817305fe4855015bc96dd62e184166330de77
Anya Petersen <anyaelena@gmail.com>
2013-12-18 10:33:57 -0700

    Only use one scheduled service.

--- poll.rb
+++ poll.rb
@@ -3,23 +3,25 @@ require 'config/initialize.rb'
 
 config = gov.nrel.bacnet.consumer.BACnet.parseOptions(ARGV)
 bacnet = gov.nrel.bacnet.consumer.BACnet.new(config)
-executorPool = bacnet.getPollingSchedSvc
+executorPool = bacnet.getSchedSvc
 local_device = bacnet.getLocalDevice
-kd = KnownDevice.where(:instance_number => 9800).first
-remote_device = kd.get_remote_device
+kd = KnownDevice.first
+# KnownDevice.all.each do |kd|
+  remote_device = kd.get_remote_device
 
-# set up polldevicetask.  this can certainly be improved
-pollone = gov.nrel.bacnet.consumer.PollDeviceTask.new(remote_device,local_device,executorPool,bacnet.getDefaultWriters)
-oids = kd.oids.where(:poll_interval_seconds.gt => -1).entries
-# this will be replaced when we save the polling offset on the known device object rather than
-# calculating it each time polling is kicked off
-oids.each do |o| 
-  pollone.addInterval(o.get_object_identifier, o.poll_interval_seconds) 
-end
+  # set up polldevicetask.  this can certainly be improved
+  pollone = gov.nrel.bacnet.consumer.PollDeviceTask.new(remote_device,local_device,executorPool,bacnet.getDefaultWriters)
+  oids = kd.oids.where(:poll_interval_seconds.gt => -1).entries
+  # this will be replaced when we save the polling offset on the known device object rather than
+  # calculating it each time polling is kicked off
+  oids.each do |o| 
+    pollone.addInterval(o.get_object_identifier, o.poll_interval_seconds) 
+  end
 
-puts "kd #{kd.instance_number} with #{oids.count} pollable oids"
-pollone.setCachedOids(oids.map{|o| o.get_object_identifier})
-delay = pollone.init()
-puts "interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
-# executorPool.scheduleWithFixedDelay(pollone, delay.getDelay, delay.getInterval, TimeUnit::SECONDS)
-executorPool.scheduleWithFixedDelay(pollone, 0, 20, TimeUnit::SECONDS)
+  puts "kd #{kd.instance_number} with #{oids.count} pollable oids"
+  pollone.setCachedOids(oids.map{|o| o.get_object_identifier})
+  delay = pollone.init()
+  puts "interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
+  executorPool.scheduleWithFixedDelay(pollone, delay.getDelay, delay.getInterval, TimeUnit::SECONDS)
+  # executorPool.scheduleWithFixedDelay(pollone, 0, 20, TimeUnit::SECONDS)
+# end
