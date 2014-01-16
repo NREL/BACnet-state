a65e5f7ed235d40d150a6b8f6b65252653ea4986
Anya Petersen <anya.petersen@nrel.gov>
2014-01-16 15:29:23 -0700

    Logging instead of puts.

--- run.rb
+++ run.rb
@@ -19,29 +19,29 @@ discoverer = Discoverer.new(config.getMinId, config.getMaxId, local_device, sche
 cur_time = Time.now.in_time_zone('Mountain Time (US & Canada)')
 midnight = cur_time.tomorrow.at_midnight
 seconds_to_midnight = midnight.to_i - cur_time.to_i
-puts "scheduling device discovery scans to run once a day at midnight.  First scan in #{seconds_to_midnight} seconds"
+LoggerSingleton.logger.info "scheduling device discovery scans to run once a day at midnight.  First scan in #{seconds_to_midnight} seconds"
 scheduler.scheduleAtFixedRate(discoverer, seconds_to_midnight, 60*60*24, TimeUnit::SECONDS)
 
 ##### OID LOOKUPS ######
 oid_discoverer = OidDiscoverer.new(local_device, filters, scheduler, sender)
-puts "scheduling OID lookup to every 10 minutes"
+LoggerSingleton.logger.info "scheduling OID lookup to every 10 minutes"
 scheduler.scheduleAtFixedRate(oid_discoverer, 0, 10*60, TimeUnit::SECONDS)
 # puts "scheduling OID lookup to run once a day 2 hours after midnight"
 # scheduler.scheduleAtFixedRate(oid_discoverer, seconds_to_midnight + 60*60*2, 60*60*24, TimeUnit::SECONDS)
 
 ##### POLLING #####
-puts "kicking off polling"
+LoggerSingleton.logger.info "kicking off polling"
 KnownDevice.all.each do |kd|
   if kd.complete?
     remote_device = kd.get_remote_device
-    polltask = PollDeviceTask.new(remote_device,local_device,bacnet.getDefaultWriters)
+    polltask = PollDeviceTask.new(remote_device,local_device,bacnet.getDefaultWriters,our_exec.getRecorderSvc)
     oids = kd.oids.where(:poll_interval_seconds.gt => -1).entries
     oids.each do |o| 
       polltask.addInterval(o.get_object_identifier, o.poll_interval_seconds) 
     end
     polltask.setCachedOids(oids.map{|o| o.get_object_identifier})
     delay = polltask.init()
-    puts "initializing polling of device #{kd.instance_number} with #{oids.count} pollable oids. interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
+    LoggerSingleton.logger.info "initializing polling of device #{kd.instance_number} with #{oids.count} pollable oids. interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
     pollone = SchedulablePoll.new(polltask, our_exec, kd)
     scheduler.scheduleAtFixedRate(pollone, delay.getDelay, delay.getInterval, TimeUnit::SECONDS)
   end
