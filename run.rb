0b5eb32fd57c24c921dd5d1acc09b1ad01043076
Anya Petersen <anya.petersen@nrel.gov>
2014-02-20 23:14:02 -0700

    Check oid count before attempting to init a PollDeviceTask object.

--- run.rb
+++ run.rb
@@ -51,14 +51,16 @@ KnownDevice.all.each do |kd|
     remote_device = kd.get_remote_device
     polltask = PollDeviceTask.new(remote_device,local_device,bacnet.getDefaultWriters,our_exec.getRecorderSvc)
     oids = kd.oids.where(:poll_interval_seconds.gt => -1).entries
-    oids.each do |o| 
-      polltask.addInterval(o.get_object_identifier, o.poll_interval_seconds) 
+    if oids.count > 1 # todo  change to > 0 when java bug polling devices with one oid is fixed
+      oids.each do |o| 
+        polltask.addInterval(o.get_object_identifier, o.poll_interval_seconds) 
+      end
+      polltask.setCachedOids(oids.map{|o| o.get_object_identifier})
+      delay = polltask.init()
+      LoggerSingleton.logger.info "initializing polling of device #{kd.instance_number} with #{oids.count} pollable oids. interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
+      pollone = SchedulablePoll.new(polltask, our_exec, kd)
+      scheduler.scheduleAtFixedRate(pollone, delay.getDelay, delay.getInterval, TimeUnit::SECONDS)
     end
-    polltask.setCachedOids(oids.map{|o| o.get_object_identifier})
-    delay = polltask.init()
-    LoggerSingleton.logger.info "initializing polling of device #{kd.instance_number} with #{oids.count} pollable oids. interval = #{delay.getInterval} and initial delay = #{delay.getDelay}"
-    pollone = SchedulablePoll.new(polltask, our_exec, kd)
-    scheduler.scheduleAtFixedRate(pollone, delay.getDelay, delay.getInterval, TimeUnit::SECONDS)
   end
 end
 
